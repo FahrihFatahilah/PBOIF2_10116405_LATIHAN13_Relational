d2a0f8392a11d3ca70827a37fa33d7baca012ff9
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-10-20 12:45:21 +0800

    PBOIF2

---
+++ src/pboif2_10116405_latihan13_relational/PBOIF2_10116405_LATIHAN13_Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116405_latihan13_relational;
+
+/**
+ *
+ * @author Fahrih Fatahilah
+ * Nama  : Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * 
+ */
+public class PBOIF2_10116405_LATIHAN13_Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b =  " + (bil1 == bil2));
+        System.out.println("a != b =  " + (bil1 != bil2));
+        System.out.println("a > b =  " + (bil1 > bil2));
+        System.out.println("a < b =  " + (bil1 < bil2));
+        System.out.println("b >= a =  " + (bil1 >= bil2));
+        System.out.println("b <= a =  " + (bil1 <= bil2));
+    }
+    
+}
